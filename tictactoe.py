bcc626ec83a5bbf286f0aa6e22c19b7cfd6652ed
Udita Alawadhi <40214762+udita-alawadhi@users.noreply.github.com>
2019-01-22 11:47:22 +0530

    Update tictactoe.py
    Changed to make the computer play smartly, and increasing the win chances of the computer

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,6 @@
 import random
 
-board={1:' ', 2:' ', 3:' ', 4:' ', 5:' ', 6:' ', 7:' ', 8:' ', 9:' '}
+board=[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
 def printBoard(board):
     print(board[1]+'|'+board[2]+'|'+board[3])
@@ -44,7 +44,7 @@ def choosePlayerMove():
         #check if input is an integer only
         empty = []
         for i in range(1,10):
-            if freespace(i):
+            if freespace(i,board):
                 empty.append(i)
         if len(empty)==0:
                 return None
@@ -62,32 +62,75 @@ def choosePlayerMove():
             else:
                 print('Please enter an integer value')
     
-def freespace(p):
-    if board[p]==' ':
+def freespace(p,bo):
+    if bo[p]==' ':
         return True
     else:
         return False
 
-    
+def duplicate(board):
+    bo=[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',' ']
+    for i in range(1,10):
+        bo[i]=board[i]
+    return bo
+
 def chooseComputerMove():
+    if comp=='X':
+        playeris='O'
+    else:
+        playeris='X'
+    
     empty = []
     for i in range(1,10):
-        if freespace(i):
+        if freespace(i,board):
             empty.append(i)
-    if len(empty)!=0:
-        return random.choice(empty)
-    else:
+    if len(empty) == 0:
         return None
+    elif len(empty)==1:
+        board[empty[0]]=comp
+        if not Winner(board, comp):
+            return None
+        else:
+            return empty[0]
+    else:
+        board2=duplicate(board)
+        for i in range(1,10):
+            if freespace(i,board2):
+                board2[i]=comp
+                if Winner(board2,comp):
+                    return i
+                else:
+                    board2[i]=' '
 
+        board3=duplicate(board)
+        for i in range(1,10):
+            if freespace(i,board3):
+                board3[i]=playeris
+                if Winner(board3,playeris):
+                    return i
+                else:
+                    board3[i]=' '
+
+        freeCorners = []
+        for i in (1,3,7,9):
+            if freespace(i,board):
+                freeCorners.append(i)
+        return random.choice(freeCorners)
+    
+        return random.choice(empty)    
+    
 def Winner(board, turn):
-    return ((board[1]==board[2]==board[3]==turn) or (board[4]==board[5]==board[6]==turn) or (board[7]==board[8]==board[9]==turn) or
-       (board[1]==board[4]==board[7]==turn) or (board[2]==board[5]==board[8]==turn) or (board[3]==board[6]==board[9]==turn) or
-       (board[1]==board[5]==board[9]==turn) or (board[3]==board[5]==board[7]==turn))
+    return ((board[1]==board[2]==board[3]==turn) or 
+            (board[4]==board[5]==board[6]==turn) or 
+            (board[7]==board[8]==board[9]==turn) or
+            (board[1]==board[4]==board[7]==turn) or 
+            (board[2]==board[5]==board[8]==turn) or 
+            (board[3]==board[6]==board[9]==turn) or
+            (board[1]==board[5]==board[9]==turn) or 
+            (board[3]==board[5]==board[7]==turn))
 
 
 play=True
-
-
 while play:
     print('-------WELCOME TO THE TIC-TAC-TOE GAME-------')
     #--start here
@@ -118,8 +161,9 @@ while play:
 
     #--Computer's turn
         if turn=='Computer':
-            move=chooseComputerMove()
+            move=chooseComputerMove(computer)
             if move==None:
+                printBoard(board)
                 print('Game tied.')
                 gameOn=False
             else:
@@ -134,8 +178,9 @@ while play:
     #---
                 
     play=playAgain()
-    board={1:' ', 2:' ', 3:' ', 4:' ', 5:' ', 6:' ', 7:' ', 8:' ', 9:' '}
-
+    board=[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+    
+    
 ''' --------------------------------------------------THIS CODE WILL RUN FOR 2 PLAYER GAME-----------------------------------------------
 print('What goes first: X or O?')
 turn=input()
